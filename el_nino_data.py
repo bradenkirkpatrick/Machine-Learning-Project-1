833344582ac25191e6cc5f9c93d0884b54b8f8ee
braden <bradenqkirk@gmail.com>
2024-10-23 22:45:11 -0400

    started pipeline and added additional feature

--- el_nino_data.py
+++ el_nino_data.py
@@ -1,5 +1,4 @@
 years = range(1951, 2025)
-print(years)
 jan_values = [
     2.5, -1.5, 0.5, 1.1, -0.9, 2.2, 1.0, -3.1, -1.5, 
     0.2, -0.4, 3.3, 1.7, -0.6, -0.7, -2.2, 2.8, 0.9, 
@@ -11,7 +10,6 @@ jan_values = [
     2.4, -1.4, -3.6, 0.3, 1.8, -0.1, .3, 3.2, 0.8, 2.3, 
     0.8
 ]
-
 feb_values = [  
     1.5, -1.0, -0.8, -0.5,
     3.1, 2.7, -0.1, -0.8, -2.3, 0.2, 1.5, -0.5, 0.9, 0.7, 
@@ -22,7 +20,6 @@ feb_values = [
     -5.2, 0.2,  -0.1, 4.4, 3.1, -2.4, 4.5, 0.8, -0.4, 0.1,
     0.4, -3.2, -0.1, -0.8, -2.3, -0.1, 2.5, 1.8, 2.3, -2.3
 ]
-print(len(years), len(jan_values), len(feb_values))
 import pandas
 df = pandas.DataFrame(data={"year": years, "jan_nino": jan_values, "feb_nino": feb_values})
 df.to_csv("./el_nino_data.csv", sep=",",index=False)
